d1fa3bb5ca7bc59acdb82796e6f86f25a12598b7
Ramsey B <Ramsey.Bissex@austintexas.gov>
2025-01-02 14:36:50 -0600

    Fix favicon; AFD logo fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ export function NavBar() {
         <nav className="navbar sticky-top border-bottom bg-body border-3">
             <div className="container-fluid">
                 <a className="navbar-brand mb-0 h1 fs-1" href="https://www.austintexas.gov/department/fire">
-                    <img src="/HazmatWebForm/hazmat/AFD.gif" alt="Logo" width="30" height="30" className="d-inline-block align-text me-3 logo" />
+                    <img src="/HazmatWebForm/AFD.gif" alt="Logo" width="30" height="30" className="d-inline-block align-text me-3 logo" />
                     Austin Fire Department
                 </a>
                 <span className="navbar-text fs-4">
